407b057aaef8e8a9150125acdb610b5d54cabd0c
Kyle Thomas <k.thomas@unmarkedconsulting.com>
2018-12-31 03:52:59 -0500

    Pipe sed expressions to munge template per aws region

--- ci-process.sh
+++ ci-process.sh
@@ -96,10 +96,7 @@ perform_deployment()
 
         echo '....file manipulation....'
         echo $ECS_TASK_DEFINITION > $DEFINITION_FILE
-        sed -E "s/node:[a-zA-Z0-9\.-]+/node:${buildRef}/" "./${DEFINITION_FILE}" > "./${MUNGED_FILE}"
-        sed -E "s/\{\{awsAccountId\}\}/${AWS_ACCOUNT_ID}/" "./${MUNGED_FILE}" > "./${MUNGED_FILE}"
-        sed -E "s/\{\{awsRegion\}\}/${awsRegion}/" "./${MUNGED_FILE}" > "./${MUNGED_FILE}"
-        cat $MUNGED_FILE
+        sed -E "s/node:[a-zA-Z0-9\.-]+/node:${buildRef}/" "./${DEFINITION_FILE}" | sed -E "s/\{\{awsAccountId\}\}/${AWS_ACCOUNT_ID}/" | sed -E "s/\{\{awsRegion\}\}/${awsRegion}/" > "./${MUNGED_FILE}"
 
         echo '....register-task-definition....'
         ECS_TASK_DEFINITION_ID=$(aws ecs register-task-definition --family "${AWS_ECS_TASK_DEFINITION_FAMILY}" --cli-input-json "file://${MUNGED_FILE}" | jq '.taskDefinitionArn' | sed -E 's/.*\/(.*)"$/\1/')
