2842180aca36fd6c72ffef16c6e6390170753db0
Kyle Thomas <kyle@provide.services>
2019-08-30 13:45:00 -0400

    Fix relativity of task definition json paths

--- scripts/ci-process.sh
+++ scripts/ci-process.sh
@@ -118,8 +118,8 @@ perform_deployment()
         echo '....[PRVD] Skipping container deployment....'
     else
         taskDefinitionFile="./ecs-task-definition.json"
-        MUNGED_FILE="${buildPath}/ecs-task-definition-${awsRegion}-UPDATED.json"
-        MUNGED_FILE_TMP="${buildPath}/ecs-task-definition-${awsRegion}.tmp.json"
+        MUNGED_FILE="./ecs-task-definition-${awsRegion}-UPDATED.json"
+        MUNGED_FILE_TMP="./ecs-task-definition-${awsRegion}.tmp.json"
 
         export AWS_DEFAULT_REGION=$awsRegion
         $(aws ecr get-login --no-include-email --region ${awsRegion})
