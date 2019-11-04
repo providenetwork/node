b18082c2ff25f9c7f8a833e88a9eec2ebe618c18
Kyle Thomas <kyle@provide.services>
2019-11-04 01:17:26 -0500

    Set CORS origin and methods on IPFS nodes

--- lib/ipfs.sh
+++ lib/ipfs.sh
@@ -13,9 +13,19 @@ then
   if [[ -z "${IPFS_GATEWAY_PORT}" ]]; then
     IPFS_GATEWAY_PORT=8080
   fi
+  
+  if [[ -z "${IPFS_CORS_ORIGINS}" ]]; then
+    IPFS_CORS_ORIGINS='["*"]'
+  fi
+
+  if [[ -z "${IPFS_CORS_METHODS}" ]]; then
+    IPFS_CORS_METHODS='["GET", "POST", "PUT", "DELETE", "OPTIONS"]'
+  fi
 
   ipfs config Addresses.API /ip4/0.0.0.0/tcp/${IPFS_API_PORT}
   ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/${IPFS_GATEWAY_PORT}
+  ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin ${IPFS_CORS_ORIGINS}
+  ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods ${IPFS_CORS_METHODS}
 
   if [ "$CLIENT" = "ipfs" ]; then
     ipfs daemon
