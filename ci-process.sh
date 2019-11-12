abccdf4019a593994379d27804cfe56204067f3d
Kyle Thomas <kyle@provide.services>
2019-11-12 00:10:37 -0500

    Fix reference to AWS_ECR_REPOSITORY_NAME

--- ci-process.sh
+++ ci-process.sh
@@ -108,10 +108,10 @@ perform_deployment()
         sudo docker push "${AWS_ACCOUNT_ID}.dkr.ecr.${awsRegion}.amazonaws.com/${AWS_ECR_REPOSITORY_NAME}:${buildRef}"
         sudo docker push "${AWS_ACCOUNT_ID}.dkr.ecr.${awsRegion}.amazonaws.com/${AWS_ECR_REPOSITORY_NAME}:latest"
 
-        sudo docker tag ${awsEcrRepoositoryName}:latest "${awsEcrRepoositoryName}:${buildRef}"
-        sudo docker tag ${awsEcrRepoositoryName}:latest "${awsEcrRepoositoryName}:latest"
-        sudo docker push "${awsEcrRepoositoryName}:${buildRef}"
-        sudo docker push "${awsEcrRepoositoryName}:latest"
+        sudo docker tag ${AWS_ECR_REPOSITORY_NAME}:latest "${AWS_ECR_REPOSITORY_NAME}:${buildRef}"
+        sudo docker tag ${AWS_ECR_REPOSITORY_NAME}:latest "${AWS_ECR_REPOSITORY_NAME}:latest"
+        sudo docker push "${AWS_ECR_REPOSITORY_NAME}:${buildRef}"
+        sudo docker push "${AWS_ECR_REPOSITORY_NAME}:latest"
     fi
 }
 
